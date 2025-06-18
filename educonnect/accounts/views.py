b9b4dffbcd245826c1a61de2b4bda6ecc3cd4285
mike kimani <kimanimichael09@gmail.com>
2025-06-18 23:17:43 +0300

    added views for accounts app

--- educonnect/accounts/views.py
+++ educonnect/accounts/views.py
@@ -1,3 +1,59 @@
 from django.shortcuts import render
-
+from rest_framework import status, generics
+from rest_framework.response import Response
+from rest_framework.decorator import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework_simplejwt.tokens import RefreshToken
 # Create your views here.
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def register(request):
+    serializer = UserRegistrationSerializer(data=request.data)
+    if serializer.is_valid():
+        user = serializer.save()
+        refresh = RefreshToken.for_user(user)
+        return Response({
+            'user': UserSerializer(user).data,
+            'tokens': {
+                'refresh': str(refresh),
+                'access': str(refresh.access_token)
+                }
+            }, status=status.HTTP_201_CREATED)
+        
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def login(request):
+    serializer = UserLoginSerializer(data=request.data)
+    if serializer.is_valid():
+        user = serializer.validated_data['user']
+        refresh = RefreshToken.for_user(user)
+        return Response({
+            'user': UserSerializer(user).data,
+            'tokens': {
+                'refresh': str(refresh),
+                'access': str(refresh.access_token)
+                }
+            # }, status=status.HTTP_200_OK)
+            })
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def logout(request):
+    try:
+        refresh_token = request.META.get('HTTP_REFRESH')
+        token = RefreshToken(refresh_token)
+        token.blacklist()
+        return Response({"message": "Logged out"}, status=status.HTTP_200_OK)
+    except Exception as e:
+        return Response({"message": "Invalid token"}, status=status.HTTP_400_BAD_REQUEST)
+    
+class UserProfileView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+    
+    def get_object(self):
+        return self.request.user

ff80119cc5bce85f659a871ae1f464b0577e9f93
mike kimani <kimanimichael09@gmail.com>
2025-06-18 23:22:25 +0300

    added assignments

--- educonnect/assignments/admin.py
+++ educonnect/assignments/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Assignment, Submission
 
 # Register your models here.
+
+@admin.register(Submission)
+class SubmissionAdmin(admin.ModelAdmin):
+    list_display = ['assignment', 'student', 'submitted_at', 'grade', 'status']
+    list_filter = ['submitted_at', 'graded_at']
+    search_fields = ['student__first_name', 'student__last_name', 'assignment__title']
